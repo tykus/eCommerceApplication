b176231ac6336d9b9d7e2cd19e2c2fdb1c546667
Brian O'Sullivan <brian.osullivan@live.com>
2012-07-16 00:16:16 +0100

    Product search & Product.Show presentation changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
 class Product < ActiveRecord::Base
+
+  # Validation of data begin sent to the Product model
+  # We must have a product name, a description and a product image
+  validates :product_name, :description, :picture, presence: true
+
+  # Ensure that every product has a valid price, i.e. > 0.00
+  validates_numericality_of :unit_price, :greater_than => 0.00
+
+
+
+
+  # Search Products Functionality
+  def self.search(search_query)
+    if search_query
+      find(:all, :conditions => ['product_name LIKE ?', "%#{search_query}%"])
+    else
+      find(:all)
+    end
+  end
 end
+
